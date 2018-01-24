2e00784cadfb7c977be7ca402cace9ac998a86c0
Alex Crichton <alex@alexcrichton.com>
2018-01-24 15:28:57 -0800

    Disable checks for libatomic for now
    For whatever reason this is failing the i686-freebsd builder in the Rust repo
    as-of this red-hot moment. The build seems to work fine without it so let's just
    remove it for now and pray there's a better fix later.
    
    Although if you're reading this and know of a better fix, we'd love to remove
    this!

--- llvm/cmake/modules/CheckAtomic.cmake
+++ llvm/cmake/modules/CheckAtomic.cmake
@@ -66,19 +66,20 @@ if(MSVC)
 elseif(LLVM_COMPILER_IS_GCC_COMPATIBLE)
   # First check if atomics work without the library.
   check_working_cxx_atomics64(HAVE_CXX_ATOMICS64_WITHOUT_LIB)
-  # If not, check if the library exists, and atomics work with it.
-  if(NOT HAVE_CXX_ATOMICS64_WITHOUT_LIB)
-    check_library_exists(atomic __atomic_load_8 "" HAVE_CXX_LIBATOMICS64)
-    if(HAVE_CXX_LIBATOMICS64)
-      list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
-      check_working_cxx_atomics64(HAVE_CXX_ATOMICS64_WITH_LIB)
-      if (NOT HAVE_CXX_ATOMICS64_WITH_LIB)
-        message(FATAL_ERROR "Host compiler must support 64-bit std::atomic!")
-      endif()
-    else()
-      message(FATAL_ERROR "Host compiler appears to require libatomic for 64-bit operations, but cannot find it.")
-    endif()
-  endif()
+  # RUST-SPECIFIC - commented out, see commit message
+  # # If not, check if the library exists, and atomics work with it.
+  # if(NOT HAVE_CXX_ATOMICS64_WITHOUT_LIB)
+  #   check_library_exists(atomic __atomic_load_8 "" HAVE_CXX_LIBATOMICS64)
+  #   if(HAVE_CXX_LIBATOMICS64)
+  #     list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
+  #     check_working_cxx_atomics64(HAVE_CXX_ATOMICS64_WITH_LIB)
+  #     if (NOT HAVE_CXX_ATOMICS64_WITH_LIB)
+  #       message(FATAL_ERROR "Host compiler must support 64-bit std::atomic!")
+  #     endif()
+  #   else()
+  #     message(FATAL_ERROR "Host compiler appears to require libatomic for 64-bit operations, but cannot find it.")
+  #   endif()
+  # endif()
 endif()
 
 ## TODO: This define is only used for the legacy atomic operations in
